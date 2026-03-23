2a9dfae6b00c2131ba65fd92f4116f393f126d14
1tc-selode <oseletye@gmail.com>
2026-03-23 11:10:30 +0100

    d

--- console/Program.cs
+++ console/Program.cs
@@ -12,17 +12,29 @@ namespace console
             List<Operett> operettek = Services.GetAll();
 
             // 1. Hány mű szerepel az adatbázisban?
-            int muSzam = 0;
+            List<int> egyediOperett = new List<int>();
+
             foreach (var o in operettek)
-                muSzam++;
-            Console.WriteLine($"1. Művek száma: {muSzam}");
+            {
+                if (!egyediOperett.Contains(o.Muid))
+                {
+                    egyediOperett.Add(o.Muid);
+                }
+            }
+            Console.WriteLine($"1. Művek száma: {egyediOperett.Count()}");
+
 
             // 2. Hány operettet mutattak be az 1920-as években?
-            int ev1920as = 0;
+            List<int> egyediOperett20asEvek = new List<int>();
             foreach (var o in operettek)
-                if (o.Ev >= 1920 && o.Ev <= 1929)
-                    ev1920as++;
-            Console.WriteLine($"2. 1920-as években bemutatottak: {ev1920as}");
+            {
+                if (!egyediOperett20asEvek.Contains(o.Muid) && o.Ev >= 1920 && o.Ev <= 1929)
+                {
+                    egyediOperett20asEvek.Add(o.Muid);
+                }
+            }
+            Console.WriteLine($"2. 1920-as években bemutatottak száma: {egyediOperett20asEvek.Count}");
+
 
             // 3. Kérjen be egy operett címet, majd írja ki az operett adatait és alkotóit.
             Console.Write("3. Adjon meg egy operett címet: ");
@@ -39,12 +51,17 @@ namespace console
             if (!talalt)
                 Console.WriteLine("Nincs ilyen operett az adatbázisban.");
 
+
             // 5. Hány operett rendelkezik 3-nál több felvonással?
-            int haromFelett = 0;
+            int haromnalTobbFelvonas = 0;
             foreach (var o in operettek)
+            {
                 if (Services.TobbMintHaromFelvonasos(o))
-                    haromFelett++;
-            Console.WriteLine($"5. 3-nál több felvonásos operettek száma: {haromFelett}");
+                {
+                    haromnalTobbFelvonas++;
+                }
+            }
+            Console.WriteLine($"5. 3-nál több felvonásos operettek száma: {haromnalTobbFelvonas}");
         }
     }
 }
