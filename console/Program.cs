16d27cead2afb0de97153dbba81728c8a70754d2
1tc-selode <oseletye@gmail.com>
2026-03-30 09:26:52 +0200

    ghghjhg

--- console/Program.cs
+++ console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using core;
 
 namespace console
@@ -12,45 +13,79 @@ namespace console
             List<Operett> operettek = Services.GetAll();
 
             // 1. Hány mű szerepel az adatbázisban?
-            List<int> egyediOperett = new List<int>();
 
-            foreach (var o in operettek)
-            {
-                if (!egyediOperett.Contains(o.Muid))
-                {
-                    egyediOperett.Add(o.Muid);
-                }
-            }
-            Console.WriteLine($"1. Művek száma: {egyediOperett.Count()}");
+            //List<int> egyediOperett = new List<int>();
+            //foreach (var o in operettek)
+            //{
+            //    if (!egyediOperett.Contains(o.Muid))
+            //    {
+            //        egyediOperett.Add(o.Muid);
+            //    }
+            //}
+
+            int egyediOperettDb = operettek
+                .Select(o => o.Muid)
+                .Distinct()
+                .Count();
+
+            //Console.WriteLine($"1. Művek száma: {egyediOperett.Count()}");
+            Console.WriteLine($"1. Művek száma: {egyediOperettDb}");
+
 
 
             // 2. Hány operettet mutattak be az 1920-as években?
-            List<int> egyediOperett20asEvek = new List<int>();
-            foreach (var o in operettek)
-            {
-                if (!egyediOperett20asEvek.Contains(o.Muid) && o.Ev >= 1920 && o.Ev <= 1929)
-                {
-                    egyediOperett20asEvek.Add(o.Muid);
-                }
-            }
-            Console.WriteLine($"2. 1920-as években bemutatottak száma: {egyediOperett20asEvek.Count}");
+            //list<int> egyedioperett20asevek = new list<int>();
+            //foreach (var o in operettek)
+            //{
+            //    if (!egyedioperett20asevek.contains(o.muid) && o.ev >= 1920 && o.ev <= 1929)
+            //    {
+            //        egyedioperett20asevek.add(o.muid);
+            //    }
+            //}
+
+            int huszasEvekOperettDb = operettek
+                .Where(o => o.Ev>=1920 && o.Ev<=1930)
+                .Select(o => o.Muid)
+                .Distinct()
+                .Count();
+
+            //Console.WriteLine($"2. 1920-as években bemutatottak száma: {egyediOperett20asEvek.Count}");
+            Console.WriteLine($"2. 1920-as években bemutatottak száma: {huszasEvekOperettDb}");
+
 
 
             // 3. Kérjen be egy operett címet, majd írja ki az operett adatait és alkotóit.
             Console.Write("3. Adjon meg egy operett címet: ");
             string keresettCim = Console.ReadLine();
-            bool talalt = false;
-            foreach (var o in operettek)
+
+            //bool talalt = false;
+            //foreach (var o in operettek)
+            //{
+            //    if (o.Cim.Equals(keresettCim, StringComparison.OrdinalIgnoreCase))
+            //    {
+            //        Console.WriteLine($"Cím: {o.Cim}, Év: {o.Ev}, Alkotó: {o.Nev}, Felvonások: {o.Felvonas}, Színház: {o.Szinhaz}");
+            //        talalt = true;
+            //    }
+            //}
+            //if (!talalt)
+            //    Console.WriteLine("Nincs ilyen operett az adatbázisban.");
+
+            List<Operett> talalt = operettek
+                .Where(o => o.Cim == keresettCim)
+                .GroupBy(o => new { o.Cim, o.Ev})
+                .Select(g => g.First())
+                .ToList();
+            if (talalt != null)
             {
-                if (o.Cim.Equals(keresettCim, StringComparison.OrdinalIgnoreCase))
+                foreach (Operett o in talalt)
                 {
-                    Console.WriteLine($"Cím: {o.Cim}, Év: {o.Ev}, Alkotó: {o.Nev}, Felvonások: {o.Felvonas}, Színház: {o.Szinhaz}");
-                    talalt = true;
+                    Console.WriteLine(o);
                 }
             }
-            if (!talalt)
-                Console.WriteLine("Nincs ilyen operett az adatbázisban.");
-
+            else
+            {
+                Console.WriteLine("nincs talalat");
+            }
 
             // 5. Hány operett rendelkezik 3-nál több felvonással?
             List<int> egyediOperett3nalTobbFelvonas = new List<int>();
--- gui/MainWindow.xaml.cs
+++ gui/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using core;
 
 namespace gui
 {
@@ -16,9 +17,51 @@ namespace gui
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Operett> operettek; 
         public MainWindow()
         {
             InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            try
+            {
+                operettek = Services.GetAll();
+
+                List<Operett> egyediMuCimek = operettek
+                    .GroupBy(o => o.Muid)
+                    .Select(g => g.First())
+                    .OrderBy(o => o.Cim)
+                    .ToList();
+
+                cbOperett.ItemsSource = egyediMuCimek;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"hoba a betolteskor: {ex.Message}");
+                throw;
+            }
+        }
+        private void cbOperett_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbOperett.SelectedItem is Operett kivalasztott)
+            {
+                tbCim.Text = kivalasztott.Cim;
+                tbEredeti.Text = $"Eredeti cím: {kivalasztott.Eredeti}";
+                tbEvszinhaz.Text = $"Bemutató: {kivalasztott.Ev}, {kivalasztott.Szinhaz}";
+                tbFelvonas.Text = $"Felvonások száma: {kivalasztott.Felvonas}";
+
+                dgAlkotok.ItemsSource = operettek
+                    .Where(o => o.Muid == kivalasztott.Muid)
+                    .Select(o => new Alkoto { Nev = o.Nev, Tipus = o.Tipus})
+                    .ToList();
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
